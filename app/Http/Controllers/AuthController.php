4358ab88372050ef56ff61995b27c082e44d114e
Ian <pristianb007@gmail.com>
2023-06-06 13:29:30 +0700

    Added JWT Token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
         return response()->json([
                 'status' => 'success',
                 'user' => $user,
-                'authorisation' => [
+                'authorization' => [
                     'token' => $token,
                     'type' => 'bearer',
                 ]
@@ -56,15 +56,10 @@ class AuthController extends Controller
         return response()->json([
             'status' => 'success',
             'user' => Auth::user(),
-            'authorisation' => [
+            'authorization' => [
                 'token' => Auth::refresh(),
                 'type' => 'bearer',
             ]
         ]);
-    }
-
-    public function test()
-    {
-        return "OK";
     }
 }
